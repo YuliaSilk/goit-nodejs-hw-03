79eed73fd92a36589005a23e302965f197cda8b5
YuliaSilk <129064716+YuliaSilk@users.noreply.github.com>
2023-12-25 21:38:37 +0200

    db

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 import app from './app.js';
+import mongoose from 'mongoose';
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+const DB_HOST='mongodb+srv://Yuliia:b5HS0FLvDMN283o5@cluster0.0xerkey.mongodb.net/db-contacts?retryWrites=true&w=majority';
+
+mongoose.connect(DB_HOST)
+.then(() => {
+  app.listen(3000, () => {
+    console.log("Server running. Use our API on port: 3000")
+  })
 })
+.catch(eroor => {
+console.log(error.message);
+process.exit(1);
+})
+
+// b5HS0FLvDMN283o5 
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000")
+// })
